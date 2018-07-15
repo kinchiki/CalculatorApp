ee2ebac1476cff86b5d9aa1ac982c2fb2ac123d9
Akito Mizuno <mznakt@gmail.com>
2018-07-16 00:44:07 +0900

    initialize label text

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        formulaLabel.text = ""
+        answerLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
