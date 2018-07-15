0a910749ab41e0f8930392a13b1b7872ba20bb3d
Akito Mizuno <mznakt@gmail.com>
2018-07-16 00:36:19 +0900

    change connection

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func clearCalculation(_ sender: UIButton) {
+    }
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+    }
+    @IBAction func inputFormula(_ sender: UIButton) {
+    }
 }
-
